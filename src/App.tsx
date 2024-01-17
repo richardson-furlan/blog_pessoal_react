b123ac32793bfae292a043224911952e98718c5b
unknown <richardsonfurlan@hotmail.com>
2024-01-17 17:04:34 -0300

    Implementando navegação com react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import Home from './paginas/home/Home';
 import './App.css';
-
+import Home from './paginas/home/Home';
+import Navbar from './components/navbar/NavBar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 
 function App() {
   return (
-   <Home />
-  );
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[40vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
 }
-
 export default App;
