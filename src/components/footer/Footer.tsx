e4c36623774aba1860f1a413a2769e4733dc65ba
unknown <richardsonfurlan@hotmail.com>
2024-01-31 10:52:06 -0300

    :rocket: Corrigindo erros do Deploy

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import { AuthContext } from '../../contexts/AuthContext'
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
-  const { usuario, handleLogout } = useContext(AuthContext)
+  const { usuario } = useContext(AuthContext)
 
   let footerComponent
 
@@ -14,7 +14,7 @@ function Footer() {
       <>
         <div className="flex justify-center bg-indigo-900 text-white">
           <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>Blog pessoal | Copyright: Richardson Furlan </p>
+            <p className='text-xl font-bold'>Blog pessoal | Copyright: Richardson Furlan - {data}</p>
             <p className='text-lg'>Acesse nossas redes sociais</p>
             <div className='flex gap-2'>
               <a href="https://www.linkedin.com/in/richardsonfurlan/" target='_blank'><LinkedinLogo size={48} weight='bold' /></a>
